5812f404527c7642f82614933949965f946c9068
ezeilo-su <ezeilo.su@gmail.com>
2021-10-18 00:03:44 +0100

    Factor in service downtime

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const axios = require('axios');
 const { json, urlencoded } = express;
 
 const app = express();
 
 const posts = {};
 
-app.use(cors());
-app.use(json());
-app.use(urlencoded({ extended: true }));
-app.use(morgan('tiny'));
-
-app.get('/posts', (req, res) => {
-  res.status(200).json(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = ({ type, data }) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -41,7 +31,20 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+
+app.get('/posts', (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent({ type, data });
   res.status(201).json({});
 });
 
@@ -51,6 +54,13 @@ app.use((err, req, res, next) => {
   console.log(err.message);
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on port 4002');
+  const res = await axios.get('http://localhost:4005/events');
+  const events = res.data;
+
+  for (let event of events) {
+    console.log(`Processing event: ${event.type}`);
+    handleEvent({ type: event.type, data: event.data });
+  }
 });
