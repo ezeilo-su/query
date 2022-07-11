e01ed3c50d1c53f0a0c21b218b342427ac363fea
Sunday Ezeilo <ezeilo.su@gmail.com>
2022-07-11 23:51:01 +0100

    User guard for null check

--- index.js
+++ index.js
@@ -58,11 +58,12 @@ app.listen(4002, async () => {
   console.log('Listening on 4002');
   try {
     const res = await axios.get('http://event-bus-srv:4005/events');
-
-    for (let event of res.data) {
-      console.log('Processing event:', event.type);
-
-      handleEvent(event.type, event.data);
+    if (res?.data?.event) {
+      for (let event of res.data) {
+        console.log('Processing event:', event.type);
+  
+        handleEvent(event.type, event.data);
+      }
     }
   } catch (error) {
     console.log(error.message);
